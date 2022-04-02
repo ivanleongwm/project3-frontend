eac1ed86eac15bc8461358a14e07ac15d9c9709e
ivanleongwm <ivanleongwm@gmail.com>
2022-04-02 15:57:16 +0800

    test

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import urlcat from "urlcat";
-
-const BACKEND = process.env.REACT_APP_BACKEND ?? "http://localhost:2000";
+import { BACKEND } from "../utils/utils";
 
 const url = urlcat(BACKEND, "/api/holidays");
 
@@ -23,7 +22,7 @@ function Create() {
           if (data.error) {
               setError(data.error)
           }
-      });
+      }).catch(error => console.log(error) );
   };
 
   const handleSubmit = (event) => {

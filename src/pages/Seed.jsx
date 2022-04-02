3f26fd3ce1c4026e52a12260bec1babc3e968c0f
ivanleongwm <ivanleongwm@gmail.com>
2022-04-02 12:13:50 +0800

    push3

--- src/pages/Seed.jsx
+++ src/pages/Seed.jsx
@@ -1,16 +1,19 @@
-const seed = [
-  {
-    name: "New Year's Day",
-  },
-  {
-    name: "Good Friday",
-  },
-];
+import { useEffect, useState } from "react";
+
+const BACKEND = process.env.REACT_APP_BACKEND ?? "http://localhost:2000" 
 
 function Seed() {
+  const [seed, setSeed] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BACKEND}/api/holidays/seed`)
+      .then((response) => response.json())
+      .then((data) => setSeed(data));
+  }, []);
+
   return (
     <>
-      <h1>Seed</h1>
+      <h1>Seed 2</h1>
       <pre>{JSON.stringify(seed, null, 2)}</pre>
     </>
   );
